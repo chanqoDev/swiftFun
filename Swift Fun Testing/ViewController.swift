6e2102fafd73a74ab39a95fc29d1ae508b55ba93
Christian Elena <chanqogetter@Christians-MacBook-Pro.local>
2017-12-12 23:08:35 -0800

    First Commit !!

--- Swift Fun Testing/ViewController.swift
+++ Swift Fun Testing/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var buttonCount = 0; // we declared a variable
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ChangeTextButton(_ sender: Any) {
+        buttonCount = buttonCount + 1; // You increment the value of the var
+        print(buttonCount); // print it to console.log
+       
+        if buttonCount >= 10{
+            view.backgroundColor = UIColor.red
+            TextLabel.text = "You hit more than 10 times!!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
